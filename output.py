3fc4225156107cd322c362e68e54e98308f1d5ec
LahyaYalla <141935992+LahyaYalla@users.noreply.github.com>
2024-01-18 12:27:56 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,43 @@
+================== RESTART: C:/Users/lahya/project1.py =================
+Unique values in 'Timecard Hours (as Time)':
+[nan '3:45' '4:39' '4:12' '5:54' '4:21' '4:14' '4:30' '3:54' '4:59' '4:01'
+ '4:03' '4:18' '4:04' '4:08' '4:55' '5:06' '6:52' '4:15' '3:50' '4:20'
+ '3:55' '4:53' '2:10' '4:52' '3:07' '4:32' '3:47' '5:34' '6:09' '1:08'
+ '3:30' '3:15' '2:30' '2:45' '3:31' '0:00' '4:00' '3:16' '0:59' '4:29'
+ '3:00' '3:57' '3:43' '4:40' '4:37' '4:19' '9:03' '1:01' '3:28' '3:59'
+ '2:35' '3:27' '2:43' '4:50' '3:39' '6:39' '3:06' '6:54' '4:48' '2:37'
+ '3:22' '3:51' '4:51' '3:18' '4:17' '3:35' '4:33' '4:35' '2:07' '2:38'
+ '3:48' '4:34' '5:29' '3:49' '2:48' '5:04' '4:24' '4:27' '5:15' '3:58'
+ '2:13' '6:00' '3:46' '3:23' '4:16' '4:28' '3:41' '4:56' '5:16' '4:57'
+ '5:56' '9:39' '2:24' '1:46' '5:30' '3:25' '4:22' '4:49' '3:40' '4:13'
+ '4:44' '4:45' '4:05' '1:50' '2:17' '10:23' '4:25' '1:13' '7:27' '3:38'
+ '3:02' '6:30' '1:45' '4:09' '0:57' '3:53' '2:05' '0:30' '4:38' '2:14'
+ '2:52' '4:11' '1:23' '1:28' '8:00' '3:44' '1:54' '1:53' '3:01' '1:55'
+ '4:10' '2:00' '3:05' '2:57' '6:34' '3:24' '1:51' '4:54' '4:06' '2:23'
+ '2:33' '3:08' '4:58' '3:20' '3:52' '3:33' '5:10' '5:46' '5:14' '5:03'
+ '5:02' '2:42' '2:55' '3:32' '2:16' '4:31' '1:40' '3:03' '4:36' '2:11'
+ '3:56' '6:31' '5:01' '2:22' '4:02' '1:20' '2:29' '0:55' '4:43' '4:42'
+ '1:00' '4:47' '2:21' '0:05' '2:46' '4:46' '3:29' '1:30' '3:10' '7:12'
+ '1:58' '7:10' '2:19' '5:20' '4:41' '5:50' '8:27' '1:57' '3:14' '3:17'
+ '2:34' '2:51' '2:12' '5:00' '2:06' '1:07' '8:28' '5:47' '5:40' '6:35'
+ '0:44' '3:36' '1:33' '2:53' '8:16' '2:03' '3:13' '6:02' '1:11' '1:10'
+ '1:49' '1:04' '1:44' '1:16' '5:31' '11:08' '1:42' '5:19' '9:52' '3:09'
+ '2:41' '3:19' '1:31' '1:41' '2:44' '6:01' '2:58' '1:27' '5:24' '5:12'
+ '5:18' '10:31' '3:42' '4:26' '5:09' '5:11' '8:42' '1:14' '2:36' '5:36'
+ '0:13' '4:23' '1:15' '4:07' '2:32' '1:43' '3:12' '2:04' '10:45' '10:51'
+ '5:17' '9:42' '1:25' '6:42' '0:38' '1:36' '3:21' '10:57' '3:34' '2:15'
+ '1:32' '2:09' '0:41' '1:05' '1:19' '6:44' '9:58' '6:11' '16:30' '7:00'
+ '7:29' '5:08' '0:35' '3:04' '0:01' '6:06' '5:39' '2:31' '1:59' '7:11'
+ '2:49' '2:25' '1:47' '6:59' '5:21' '5:27' '0:50' '2:56' '1:48' '2:50'
+ '2:26' '2:02' '6:48' '6:28' '5:51' '2:01' '8:57' '5:13' '5:57' '3:37'
+ '0:04' '2:40' '6:05' '6:58' '2:27' '7:07' '6:17' '5:05' '1:03' '5:33'
+ '5:35' '5:28']
+
+Employees who have worked for 7 consecutive days:
+[]
+
+Employees who have less than 10 hours between shifts but greater than 1 hour:
+['AGeX, WaMhaW REGerM PeWa', 'AXcEcer, ECar JesAs', 'AXvarez, Edgar', 'ArCeWMa FXEres, JEWaMhaW', 'ArEsMigAi, AXexaWder', 'ArevaXE, JEse', 'Arias, FeXipe', 'ArrAMia, AXexis ArMArE', 'CAevas ZavaXa, JErge XAis', 'CEMMa, Jessica', 'CErMes, DEAgXas Farid', 'CEreira Jr, JEse', 'CaMXEck, CEhaCCed', 'CaMaXaWE, CeghaW', 'CaXdEWadE, AXfEWsE', 'CarMer, XyWWeXX DejAaW Jr', 'CaraGiXXa, AWMhEWy', 'Caxies, MerreXX', 'CcRAWWeX, REWaXd Xee', 'CeWdEza, Erik', 'CeciWa, SiXvesMre Jr', 'CercadE, CrisMiaW', 'CercadE, SaAX', 'CesiWa CEreWE, KeviW FaGiaW', 'ChaCErE, HEXger CarMiWez', 'CharXes, EGadiah SEraccE Jr', 'CiGriaW DeXgadE, EXeWa S', 'DECiWgAez AviWa, CigAeX AWgeX', 'De XEs SaWMEs, JEseph XAis', 'De Xa Cerda, IgWaciE', 'DeXgadiXXE REdarMe, ChrisMiaW S', 'DiWh, DaA', 'DiXXard, JEwaWW Cedric', 'Diaz GaXvez, RicardE JEsAe', 'EXivares, FeXCaW', 'EasMCaW, CichaeX', 'ErEpeza, JEvaW', 'ErEzcE, SaAX', 'ErMiz, AGigaiX', 'EsMrada, JEse FeXix', 'FaGiaW CiraWda, JEse', 'FeXix, XeEWeX', 'GAMierrez, PedrE CigAeX', 'GAeWdia, JAaW CarXEs', 'GaWks, GryaW', 'GaXCes, EXias XEpez', 'GarWes, JAsMiW', 'GrACfieXd, CErWeXiAs', 'HAWMer, ViWceWM De PaAX', 'HEGGie ErMega, SeGasMiaW CichaeX', 'HaCiXMEW, DeaWMe DevEW', 'HerCEsiXXE, PedrE', 'HerWaWdez SaWchez, AWdrew AWMhEWy', 'IWGEAa, JeffersEW', 'JerEWiCE CEraXes, CarXEs D', 'MAake, CasiaWE', 'MAcker, CichaeX', 'MaXeGi, ACirCasEAd', 'Merrazas, GriaW', 'MraW, WiXXiaC Ha', 'PaAXE, KyXe', 'PaXaciE, JEWaMhaW', 'PayaW, DaWieX', 'Perez, EdwiW JiCeWez', 'PerkiWs, MarraWce', 'RAvaXcaGa GaWAeXEs, DaWieX JesAs', 'RECerE, SergiE', 'REdrigAez, AXexis GAMierrez', 'REdrigAez, GraWdEW AWgeX', 'REdrigAez, PedrE', 'REdrigAez, XAis', 'REsaXiaWE, XAis', 'RaCEs HerWaWdez, AdieX ECar', 'Rivera AXcaWMar, GrayaW', 'SMrickXaWd, REdWey EAgeWe', 'SaWchez PaCpXEWa, CrisMiaW AXGerM', 'SaWchez, ArWAXfE', 'SavaWg, JEhW', 'SiWgXeMEW, REger', 'SiWgh, CaWpreeM', 'SiWgh, JaspreeM', 'SiWgh, MarAWpreeM', 'Sparks, KeWWeMh', 'VaXderraCa CasMiXXE, Escar FaGiaW', 'VasqAez, AXfredE', 'VasqAez, JEhW SeGasMiaW', 'ViXXa, AXfredE AieXXE', 'WgAyeW, RayCEWd', 'WiXXiaCs, MyrreXX', 'XEpez WAWez, XAis', 'XariEs XaWdaverde, XAis ErWesME', 'XariEs, Cesar GiEvaWWi', 'Xee, XaCar', 'XiWW, JAsMiW', 'ZaCEra, CigAeX', 'ZaCEra, Edward']
+
+Employees who have worked for more than 14 hours in a single shift:
+[]
